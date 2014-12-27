1da9929652eb868001e449611e588fc9f9ff3fd4
Albin Larsson <albin.post@gmail.com>
2014-12-27 16:49:44 +0100

    Frontend
    Faked backend stuff

--- core/config.php
+++ core/config.php
@@ -1,1 +1,5 @@
 <?php
+define('HOST', '127.0.0.1'); // the IP of the database
+define('DBNAME', 'bildplats'); // the database name to be used
+define('USERNAME', 'root'); // the username to be used with the database
+define('PASSWORD', ''); // the password to be used with the username

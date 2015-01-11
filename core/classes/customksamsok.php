ab45519e517bf4016962ec12c8d98580d96d3798
Albin Larsson <albin.post@gmail.com>
2015-01-11 13:23:01 +0100

    Extend K-Samsök class

---
+++ core/classes/customksamsok.php
@@ -0,0 +1,21 @@
+<?php
+class customKSamsok extends kSamsok {
+  public function photoSearch($text) {
+    // build $urlQuery
+    $urlQuery = $this->url . 'x-api=' . $this->key . '&method=search&hitsPerPage=250&query=itemType="Foto"%20and%20text="' . $text . '"%20and%20mediaType="image/jpeg"%20and%20thumbnailExists=j&recordSchema=presentation';
+    $urlQuery = utf8_decode($urlQuery);
+    $this->validXml($urlQuery);
+    // get the XML
+    $xml = file_get_contents($urlQuery);
+    // instead of using XPath to parse RDF just by pass it
+    $xml = $this->killXmlNamespace($xml);
+    $xml = new SimpleXMLElement($xml);
+
+    // parse each record and push to $result array
+    foreach ($xml->records->record as $record) {
+      $result[] = $this->parseRecord($record);
+    }
+
+    return $result;
+  }
+}

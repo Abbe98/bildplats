124e7197262ea135e57018e896f5998fe4c4b906
Albin Larsson <albin.post@gmail.com>
2015-01-31 12:30:22 +0100

    scale images and result container

--- frontend/ui.js
+++ frontend/ui.js
@@ -11,7 +11,26 @@ function createMap() {
 var windowHeight = $(window).height();
 $('.container').css('height', windowHeight);
 
-$( window ).resize(function() {
+function centerContent() {
+  setTimeout(function() {
+    var contentHeight = $('#result_wrapper').height();
+    var containerHeight = $('.container').height();
+
+    var imageMaxHeight = containerHeight - 160;
+    $('#image_holder').css('max-height', imageMaxHeight);
+
+    if (contentHeight < containerHeight) {
+      var padding = containerHeight - contentHeight;
+      padding = padding / 2;
+
+      $('#result_wrapper').css('padding-top', padding);
+    } else {
+      $('#result_wrapper').css('padding-top', '0px');
+    }
+  }, 300);
+}
+
+$(window).resize(function() {
   var windowHeight = $(window).height();
   $('.container').css('height', windowHeight);
 });

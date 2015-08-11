882696acafe3a8a129f4481c2ed2d807b0fa14e8
Albin Larsson <albin.post@gmail.com>
2015-08-11 20:21:40 +0200

    setup fake auth function for development :factory:

---
+++ core/classes/user.php
@@ -0,0 +1,11 @@
+<?php
+#TODO
+// All(OAuth) wikimedia stuff should go in here
+class User {
+  #TODO
+  // this should check sessions and stuff... 
+  // for now just pass parameter(true||false)
+  public static function authorized($bool = true) {
+    return $bool;
+  }
+}

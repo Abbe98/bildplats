eefcb6798d2d8a681420b67b446f685cb9de2f4e
Albin Larsson <albin.post@gmail.com>
2015-09-01 11:07:31 +0200

    Bounding box API :cake:
    closing #1

--- api.php
+++ api.php
@@ -10,6 +10,7 @@ if (isset($_GET['method'])) {
     // if the URI is valid
     if ($uri !== false) {
       $result = $db::getObject($uri);
+
       if (!$result) {
         apiError('An object with this URI does not exists in the database.');
       } else {
@@ -18,6 +19,27 @@ if (isset($_GET['method'])) {
       }
     } else {
       apiError('Invalid URI or no URI parameter set.');
+    }
+  } elseif ($_GET['method'] === 'box') {
+    $box[] = $_GET['south'];
+    $box[] = $_GET['east'];
+    $box[] = $_GET['north'];
+    $box[] = $_GET['west'];
+
+    foreach ($box as $coord) {
+      if (!is_numeric($coord)) {
+        apiError('Invalid parameters or parameter values.');
+        die();
+      }
+    }
+
+    $result = $db::boxSearch($box);
+
+    if (!$result) {
+      apiError('No objects exist within this bounding box.');
+    } else {
+      echo json_encode($result);
+      die();
     }
   } else {
     apiError('Invalid API method.');

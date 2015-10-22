97658a7630dc455aaa246b1e4f1004466b534213
Albin Larsson <albin.post@gmail.com>
2015-10-22 09:31:08 +0200

    First work on image view

---
+++ image.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'core/init.php';
+
+if (isset($_GET['uri'])) {
+  $uri = $KSamsok->uriFormat($_GET['uri'], 'rawurl');
+  if ($uri !== false) {
+    $result = db::getObject($uri);
+    if (!$result) {
+      #TODO
+      // make uri search
+    } else {
+      // request object using KSamsok->object()
+    }
+  } else {
+    #TODO
+    // 404 page
+    echo '404';
+  }
+} else {
+  #TODO
+  // 404 page
+  echo '404';
+}
+?>
+<!doctype html>
+<html lang="sv">
+	<head>
+		<title><?php echo $KSamsok->uriFormat($result['kosamsok'], 'raw'); ?></title>
+		<meta charset="UTF8" />
+
+		<link rel="stylesheet" href="frontend/style.css" />
+		<link rel="stylesheet" href="https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.css" />
+	</head>
+	<body>
+
+		<script src="frontend/bildplats.js"></script>
+		<script src="frontend/ui.js"></script>
+		<script src="frontend/app.js"></script>
+	</body>
+</html>
